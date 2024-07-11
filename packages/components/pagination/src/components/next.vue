8b86ac6185288b3c40d1d9c7df8084232231dad7
qyqiang <yingqiang.qi@beyondtrucks.com>
2024-07-11 11:43:48 +0800

    feat: table and pagination ui

--- packages/components/pagination/src/components/next.vue
+++ packages/components/pagination/src/components/next.vue
@@ -9,7 +9,23 @@
   >
     <span v-if="nextText">{{ nextText }}</span>
     <el-icon v-else>
-      <component :is="nextIcon" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="13"
+        height="12"
+        viewBox="0 0 13 12"
+      >
+        <g clip-path="url(#clip0_1345_15810)">
+          <path
+            d="M4.5635 12L9.317 7.24C9.64407 6.91139 9.82768 6.46663 9.82768 6.003C9.82768 5.53937 9.64407 5.09461 9.317 4.766L4.558 0L3.5 1.0605L8.259 5.826C8.30587 5.87288 8.3322 5.93646 8.3322 6.00275C8.3322 6.06904 8.30587 6.13262 8.259 6.1795L3.505 10.9395L4.5635 12Z"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_1345_15810">
+            <rect width="12" height="12" transform="translate(0.5)" />
+          </clipPath>
+        </defs>
+      </svg>
     </el-icon>
   </button>
 </template>
--- packages/components/pagination/src/components/prev.vue
+++ packages/components/pagination/src/components/prev.vue
@@ -9,7 +9,23 @@
   >
     <span v-if="prevText">{{ prevText }}</span>
     <el-icon v-else>
-      <component :is="prevIcon" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="13"
+        height="12"
+        viewBox="0 0 13 12"
+      >
+        <g clip-path="url(#clip0_1345_15785)">
+          <path
+            d="M8.52052 12L3.76702 7.24C3.43996 6.91139 3.25634 6.46663 3.25634 6.003C3.25634 5.53937 3.43996 5.09461 3.76702 4.766L8.52602 0L9.58502 1.0605L4.82602 5.826C4.77916 5.87288 4.75283 5.93646 4.75283 6.00275C4.75283 6.06904 4.77916 6.13262 4.82602 6.1795L9.57902 10.9395L8.52052 12Z"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_1345_15785">
+            <rect width="12" height="12" transform="translate(0.5)" />
+          </clipPath>
+        </defs>
+      </svg>
     </el-icon>
   </button>
 </template>
