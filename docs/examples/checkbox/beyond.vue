784dca52eddef373add12b824d000decdbb4a8b3
Zhang Juntao <zhangjuntao@ZhangdeMacBook-Pro.local>
2025-01-16 09:11:05 +0800

    style: BT3-2 checkbox

---
+++ docs/examples/checkbox/beyond.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <el-checkbox />
+    <el-checkbox v-model="checked2" disabled />
+    <el-checkbox v-model="checked3" disabled />
+    <el-checkbox :error="isError" />
+    <el-checkbox :indeterminate="isIndeterminate" />
+  </div>
+  <el-divider />
+  <div>
+    <el-checkbox v-model="checked1" label="Option 1" />
+    <el-checkbox v-model="checked2" label="Option 2" disabled />
+    <el-checkbox v-model="checked3" label="Option 3" disabled />
+    <el-checkbox label="Error" :error="isError" />
+    <el-checkbox :indeterminate="isIndeterminate">Indeterminate </el-checkbox>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const isIndeterminate = ref(true)
+const isError = ref(true)
+const checked1 = ref(false)
+const checked2 = ref(false)
+const checked3 = ref(true)
+</script>
