d67b7a20dda70e6f8fda508ecfbdd63ff85ffc24
qyqiang <yingqiang.qi@beyondtrucks.com>
2026-01-23 18:12:28 +0800

    fix: docs file

--- docs/.vitepress/vitepress/components/navbar/vp-menu.vue
+++ docs/.vitepress/vitepress/components/navbar/vp-menu.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
-import { useNav } from '../../composables/nav'
 import VPMenuLink from './vp-menu-link.vue'
 
-const navs = useNav()
+const navs = [
+  {
+    text: 'Component',
+    link: '/en-US/component/overview',
+    activeMatch: '/component/',
+  },
+]
 </script>
 
 <template>
